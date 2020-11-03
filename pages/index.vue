ed59886eb84e08cb7a1c06cff223527f83cd07ad
Lucky Adogun <meetluckyadogun@gmail.com>
2020-11-03 16:44:48 +0100

    large added

--- pages/index.vue
+++ pages/index.vue
@@ -11,5 +11,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Homemade+Apple&family=PT+Mono&family=Quicksand:wght@600;700&display=swap');
 
+*, body {
+  font-family: 'PT Mono', monospace;
+  font-family: 'Quicksand', sans-serif;
+}
 </style>
