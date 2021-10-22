d33d0a4dee36933759812fb2ccd02454b10ab20b
Lucky Adogun <meetluckyadogun@gmail.com>
2021-10-22 23:11:58 +0100

    adding cards

---
+++ components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+    <a href="" class="text-center p-5 bg-white my-10 rounded-3xl text-black flex flex-col justify-center">
+        <div class="flex justify-end">
+            <p class="bg-theme-dark mb-5 py-1 px-2 text-xs w-20 text-orange-500 rounded-3xl font-bold">{{cardData.price}}</p>
+        </div>
+        <div class="flex flex-col items-center">
+            <img :src="cardData.photo" class="w-2/3 mb-3" alt="">
+            <p class="font-extrabold tracking-tighter leading-tight text-xl">
+                {{ cardData.title }}
+            </p>
+            <p class="text-xs my-5">
+                {{ cardData.body }}
+            </p>
+            <p class="underline text-sm font-bold text-orange-500">Learn More</p>
+        </div>
+    </a>
+</template>
+
+
+<script>
+import cardImage from '~/assets/images/07.png'
+
+export default {
+    props: {
+        cardData: Object
+    },
+    data() {
+        return {
+            cardImage: cardImage
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,132 @@
 <template>
-  <div class="flex h-full">
+  <div class="flex flex-col h-full">
     <div class="flex flex-col items-center justify-center justify-items-center m-auto">
-        <div class="w-24 h-24 bg-white flex items-center rounded-full">
-          <img class="h-20 w-20 mx-auto" src="https://lnmlpexiwaspywjbwwvd.supabase.in/storage/v1/object/sign/assets/boy.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhc3NldHMvYm95LnBuZyIsImlhdCI6MTYzMzUzMzk2NywiZXhwIjoxOTQ4ODkzOTY3fQ.HL6DqbdJMCZI6dqEN-ZQAu5EwtblW7r8YNuud4_kHV8">
-        </div>
-        <h1 class="my-3 text-lg font-bold">Hi, I'm Lucky 👋</h1>
-        <h1 class="my-3 text-4xl md:text-5xl lg:text-6xl lg:px-56 tracking-tighter leading-tight md:leading-none text-center font-bold">
-          I build SaaS and Enterprise Products with Python & JS
+        <img class="inline object-cover w-24 h-24 mr-2 rounded-full bg-white" :src="mainPhoto"/>
+        <h1 class="my-5 text-lg font-bold">Hi, I'm Lucky 👋</h1>
+        <h1 class="my-5 text-3xl md:text-5xl lg:text-6xl lg:px-56 tracking-tighter leading-tight md:leading-none text-center font-bold">
+          Everything You Need to Build & Launch Low-Cost 
+          Scalable SaaS MVPs 10X Faster
         </h1>
-        <p class="my-3 text-xs md:text-sm lg:text-lg tracking-tight md:px-10 lg:px-56 text-center font-normal text-theme-secondary">
+        <p class="my-5 text-xs md:text-sm lg:text-lg tracking-tight md:px-10 lg:px-56 text-center font-semibold text-theme-secondary">
           I have done it many times, for many clients, even for the
           <span class="font-bold text-white">Federal Government of Nigeria <br class="hidden md:block">for the Ministry of Budget and Urban Planning. </span>
           If they can trust me, maybe you can. So feel free to reach out.
         </p>
-        <Button class="my-5" cta='say hello' />
-        <div>
-          <chevrons-down-icon size="3x" class="custom-class"></chevrons-down-icon>
+        <Button class="my-5" cta='Ask Me Anything' />
+        
+        
+        <!-- Cards -->
+        <div id="cards" class="my-10">
+          <h1 class="text-center font-bold tracking-tight leading-tight text-lg">
+              Take any of the steps below.
+              Move from wanting to doing.
+          </h1>
+
+          <div>
+            <Card :cardData="cardData1" />
+            <Card :cardData="cardData2" />
+            <Card :cardData="cardData3" />
+            <Card :cardData="cardData4" />
+          </div>
         </div>
-        <p class="text-xs text-theme-secondary my-3 underline">Click here to download my resume</p>
+
+        <div class="text-center mb-10">
+          <p class="italic font-semibold">Want to hire me instead?</p>
+          <Button class="my-5" cta='Download my resume' />
+        </div>
+
+        <!-- <p class="text-xs text-theme-secondary my-3 underline">Click here to download my resume</p>
         <p class="text-xs text-orange-500 mb-5 font-semibold">OR</p>
-        <SocialMedia />
+        <SocialMedia /> -->
     </div>
+
+    <!-- <div class="bg-white p-5 w-full">
+      <p class="text-black">
+
+        DISCLAIMER:
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tem por incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+    </div> -->
     
   </div>
 </template>
 
 <script>
 import { ChevronsDownIcon } from 'vue-feather-icons'
+
+import mainPhoto from '~/assets/images/zencoder__dark.png'
+import cardImage1 from '~/assets/images/07.png'
+import cardImage2 from '~/assets/images/02.png'
+import cardImage3 from '~/assets/images/08.png'
+import cardImage4 from '~/assets/images/09.png'
+
 export default {
   data() {
       return {
-        title: 'Home'
+        title: 'Home',
+        mainPhoto: mainPhoto,
+        cardData1: {
+          price: "$1500",
+          photo: cardImage1,
+          title: "How long will my SaaS project take and how much would it cost?",
+          body: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          `
+        },
+        cardData2: {
+          price: "FREE",
+          photo: cardImage2,
+          title: `
+                  What’s the best tech-stack 
+                  to go from idea to MVP 10X Faster 
+                  with little or no trade-offs?
+                `,
+          body: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          `
+        },
+        cardData3: {
+          price: "$2500",
+          photo: cardImage4,
+          title: `
+            What Product Roadmap should I take with less opportunity cost?
+          `,
+          body: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          `
+        },
+        cardData4: {
+          price: "$500",
+          photo: cardImage3,
+          title: `
+            I have several prospective 
+            developers with equally attractive 
+            CV’s whom should I hire?
+          `,
+          body: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          `
+        },
       }
   },
   components: {
