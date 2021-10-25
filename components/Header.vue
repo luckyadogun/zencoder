9612642dd7f279ca90f984d234a4f8efcd2218d4
Lucky Adogun <meetluckyadogun@gmail.com>
2021-10-25 15:35:42 +0100

    adding blog link

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
         <Slide class="md:hidden">
             <nuxt-link to="/" class="mr-4">Home</nuxt-link>
             <nuxt-link to="about" class="mr-4">About Me</nuxt-link>
-            <nuxt-link to="/" class="mr-4">Blog <small class="ml-1 flex items-center text-xs">- FOR DEVS</small></nuxt-link>
+            <a target="_blank" href="https://blog.zencoder.xyz" class="mr-4">Blog <small class="ml-1 flex items-center text-xs">- FOR DEVS</small></a>
             <nuxt-link to="/" class="mr-4">Build Tools <small class="ml-1 flex items-center text-xs">- FOR DEVS</small></nuxt-link>
             <nuxt-link to="/" class="mr-4">Courses <small class="ml-1 flex items-center text-xs">- FOR FOUNDERS</small></nuxt-link>
         </Slide>        
